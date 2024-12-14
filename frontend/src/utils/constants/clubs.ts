2640f1d5d690d0d209ee0cfc6c45adf2b066512d
shundayo131 <shundayo.131@gmail.com>
2024-12-14 15:44:09 -0800

    updated folder structures

---
+++ frontend/src/utils/constants/clubs.ts
@@ -0,0 +1,30 @@
+export const GOLF_CLUBS = {
+  // Woods
+  driver: false,            // Driver (1-wood)
+  "3-wood": false,         // 3-wood
+  "5-wood": false,         // 5-wood
+  
+  // Utility/Hybrid
+  "2-utility": false,      // 2-iron replacement
+  "3-utility": false,      // 3-iron replacement
+  "4-hybrid": false,       // 4-iron replacement
+  "5-hybrid": false,       // 5-iron replacement
+
+  // Irons
+  "3-iron": false,
+  "4-iron": false,
+  "5-iron": false,
+  "6-iron": false,
+  "7-iron": false,
+  "8-iron": false,
+  "9-iron": false,
+
+  // Wedges
+  "pw": false,             // Pitching Wedge (45-48°)
+  "gw": false,             // Gap/Approach Wedge (50-52°)
+  "sw": false,             // Sand Wedge (54-56°)
+  "lw": false,             // Lob Wedge (58-60°)
+
+  // Putter
+  putter: false
+};
